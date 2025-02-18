aa76a0104636a4b4ee7c0f241f3bddae010df493
nvimtor <gitfromvitor@proton.me>
2025-02-18 12:08:05 +0000

    Massive

--- org-harvest.el
+++ org-harvest.el
@@ -33,20 +33,60 @@
 (require 'cl-lib)
 
 ;; NOTE this is required, see https://github.com/minad/consult/issues/1149
-(setq consult-async-min-input 0)
-
+;; (setq consult-async-min-input 0)
 ;; NOTE will affect other packages!
 ;; (setq consult-async-split-style nil)
 
-(defvar org-harvest--projects-cache '() "Recently fetched projects from Harvest.")
+;;;;;;;;;;;;
+;; custom ;;
+;;;;;;;;;;;;
 
-(defvar org-harvest-cmd
-  (list shell-file-name shell-command-switch "./result/bin/org-harvest"))
+(defgroup org-harvest nil
+  "Customization group for the org-harvest.el package."
+  :group 'tools
+  :prefix "org-harvest-")
+
+(defcustom org-harvest-cmd
+  (list shell-file-name shell-command-switch "./result/bin/org-harvest")
+  :group 'org-harvest)
+
+;;;;;;;;;;;;;;
+;; internal ;;
+;;;;;;;;;;;;;;
+
+(defvar org-harvest--projects-cache '() "Recently fetched projects from Harvest.")
 
 (defvar org-harvest--process-name
   "org-harvest-get-tasks")
 
 (defvar org-harvest--output-buffer-name "*org-harvest-output*")
+
+(defun org-harvest--parse-line (line)
+  "Parse a CSV line into a list of fields.
+Assumes every field is enclosed in double quotes, fields are separated by commas,
+and any double quotes inside a field are escaped as \"\"."
+  (let ((fields '()))
+    (while (string-match "^\"\\(\\(?:[^\"]\\|\"\"\\)*\\)\"\\(,\\|$\\)" line)
+      (let ((field (match-string 1 line)))
+        (setq field (replace-regexp-in-string "\"\"" "\"" field))
+        (push field fields)
+        (setq line (substring line (match-end 0)))))
+    (nreverse fields)))
+
+(defun org-harvest--propertize-line (line)
+  "Parse LINE into fields, then attach them as text properties on the returned string.
+Returns the same LINE (string) with text properties storing each field:
+org-harvest-0, org-harvest-1, ..., org-harvest-4."
+  (let* ((fields (org-harvest--parse-line line)))
+    (add-text-properties
+     0 (length line)
+     (list 'org-harvest-0 (nth 0 fields)
+           'org-harvest-1 (nth 1 fields)
+           'org-harvest-2 (nth 2 fields)
+           'org-harvest-3 (nth 3 fields)
+           'org-harvest-4 (nth 4 fields))
+     line)
+    line))
 
 (defun org-harvest--get-output-buffer ()
   (cl-letf*
@@ -58,10 +98,8 @@
         (setq-local exec-path (default-value 'exec-path)))
       buf)))
 
-(defun org-harvest--group-fn (cand transform)
-  "Group" (message "%s %s" cand transform) "all")
-
-(defun org-harvest--sentinel (proc event cb)
+(defun org-harvest--get-tasks-async-sentinel (proc event cb)
+  (ignore event)
   (when (eq 0 (process-exit-status proc))
     (with-current-buffer (process-buffer proc)
       (goto-char (point-min))
@@ -83,7 +121,7 @@ clientid,projectid,taskid"
      :buffer process-buffer
      :command cmd
      :noquery t
-     :sentinel (lambda (proc event) (org-harvest--sentinel proc event cb)))))
+     :sentinel (lambda (proc event) (org-harvest--get-tasks-async-sentinel proc event cb)))))
 
 (defun org-harvest--get-tasks-sync ()
   "Return tasks from Harvest as a list of strings, synchronously."
@@ -103,18 +141,47 @@ clientid,projectid,taskid"
             (split-string (buffer-string) "\n" t)))
       (kill-buffer buf))))
 
-(defun org-harvest--action (action cand)
-  (message "elected: %s with action: %s" cand action))
+(defun org-harvest--tasks-group (cand transform)
+  "Function for Consult to group or transform a candidate string with text properties."
+  (let* ((clientname (get-text-property 0 'org-harvest-0 cand))
+         (projname   (get-text-property 0 'org-harvest-2 cand))
+         (taskname   (get-text-property 0 'org-harvest-4 cand)))
+    (if (not transform)
+        (format "%s (%s)" clientname projname)
+      taskname)))
 
-(defun org-harvest-read-projects ()
+;; NOTE 'return action doesn't preserve cand's text properties https://github.com/minad/consult/issues/582
+(defun org-harvest--tasks-state (action cand)
+  (pcase action
+    ('return
+     (let* ((projid (get-text-property 0 'org-harvest-1 cand))
+            (taskid (get-text-property 0 'org-harvest-3 cand)))
+       (org-entry-put nil "HARVEST_PROJECT_ID" projid)
+       (org-entry-put nil "HARVEST_TASK_ID" taskid)))))
+
+(defun org-harvest--tasks-lookup (selected cands &rest _)
+  (car (member selected cands)))
+
+;;;;;;;;;;;;
+;; public ;;
+;;;;;;;;;;;;
+
+;;;###autoload
+(defun org-harvest-tasks ()
   (interactive)
   (consult--read
    (consult--dynamic-collection
-    (lambda (input cb)
-      (ignore input)
-      (let ((tasks (org-harvest--get-tasks-sync)))
-        (funcall cb tasks))))
-   :state 'org-harvest--action
-   :group 'org-harvest--group-fn))
+       (lambda (input cb)
+         (ignore input)
 
-;;; scratch.el ends here
+         (funcall cb org-harvest--projects-cache)
+
+         (let* ((tasks (org-harvest--get-tasks-sync))
+                (propertized (mapcar #'org-harvest--propertize-line tasks)))
+           (setq org-harvest--projects-cache propertized)
+           (funcall cb propertized))))
+   :state 'org-harvest--tasks-state
+   :lookup 'org-harvest--tasks-lookup
+   :group 'org-harvest--tasks-group))
+
+;;; org-harvest.el ends here
